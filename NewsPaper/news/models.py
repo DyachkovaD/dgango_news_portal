f3bda10436d93ced120cad12f651cbe5993e246b
Darya Dyachkova <d.dyachkova@bk.ru>
2024-07-15 17:28:44 +0300

    the final changing maybe

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-from functools import reduce
-
+from django.db.models import Sum
+from django.db.models.functions import Coalesce
 
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=0)  # !!!!!
+    rating = models.IntegerField(default=0)
 
-    # def update_rating(self):
-    #     posts_rating = self.post_set.all().values('rating')
-    #     posts_rating_sum = reduce(lambda x, y: x + y, map(lambda x: x.get('pk'), posts_rating)) * 3
-    #
+    def update_rating(self):
+        posts_rating = self.post_set.aggregate(pr=Coalesce(Sum('rating'), 0))['pr']
+        comments_rating = self.user.comment_set.aggregate(cr=Coalesce(Sum('rating'), 0))['cr']
+        post_comments_rating = self.post_set.aggregate(pcr=Coalesce(Sum('comment__rating'), 0))['pcr']
 
-
-
+        self.rating = posts_rating + comments_rating * 3 + post_comments_rating
+        self.save()
 
 
 class Category(models.Model):
@@ -26,7 +26,7 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0)  # !!!!!
+    rating = models.IntegerField(default=0)
 
     def like(self):
         self.rating += 1
@@ -51,7 +51,7 @@ class Post(models.Model):
     category = models.ManyToManyField(Category, through='PostCategory')
     title = models.CharField(max_length=225)
     text = models.TextField()
-    rating = models.IntegerField(default=0)  # !!!!
+    rating = models.IntegerField(default=0)
 
     def like(self):
         self.rating += 1
@@ -68,4 +68,3 @@ class Post(models.Model):
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
